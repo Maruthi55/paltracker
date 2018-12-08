4a0b3b2ce79d8de9199b5f352cfb65e0f20d987e
Pullagura Maruthi <pullagura.maruthi@Perficient.com>
2018-12-08 17:54:14 +0530

    travis task committed

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class WelcomeController {
-    String message;
 
-    public WelcomeController(@Value("${WELCOME_MESSAGE}") String message) {
-        this.message = message;
+    private String message;
+
+
+    public WelcomeController( @Value("${WELCOME_MESSAGE}")  String msg){
+        message=msg;
     }
 
     @GetMapping("/")
